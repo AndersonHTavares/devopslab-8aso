8959166eb340272a6bd5dca9329a928e7aaccbda
Anderson Tavares <31394163+AndersonHTavares@users.noreply.github.com>
2022-10-10 18:44:51 -0300

    update Texto para gravação

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO v02"
+    return "Laboratório DevOps - FIAP 8ASO RM343515 - Anderson Tavares"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
