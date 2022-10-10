164c2cb2f752d6ce5e3b314f82e18e7f7a568f6b
Anderson Tavares <31394163+AndersonHTavares@users.noreply.github.com>
2022-10-10 20:00:26 -0300

    update test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO (Anderson Tavares - RM343515) Fase Final , Gabi Parabéns pela Didática!!!"
+    return "Laboratório DevOps - FIAP 8ASO (Anderson Tavares - RM343515) Fase Final"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
